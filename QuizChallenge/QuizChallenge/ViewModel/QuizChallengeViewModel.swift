55888ed85fb69ca4b93685cd4b06d82aff0467b2
Fernanda de Lima <limade.f@gmail.com>
2020-01-30 03:20:04 -0300

    bateria do Mac acanbando, salvando tudo

--- QuizChallenge/QuizChallenge/ViewModel/QuizChallengeViewModel.swift
+++ QuizChallenge/QuizChallenge/ViewModel/QuizChallengeViewModel.swift
@@ -11,6 +11,59 @@ import UIKit
 
 class QuizChallengeViewModel{
     var showLoadingHud: Bindable = Bindable(false)
+    var onCountAnswer: Bindable = Bindable("")
+    var updateTimer: Bindable = Bindable("05:00")
+    var onChallengeLoad: Bindable<String?> = Bindable(nil)
+    var onShowAlert: Bindable<UIAlertController?> = Bindable(nil)
+    var answer: [String] = []
+    var playerAnswer: [String] = []{
+        didSet{
+            onCountAnswer.value = "\(self.playerAnswer.count)/\(self.answer.count)"
+        }
+    }
     
-    var awser: [String] = []
+    private lazy var countdown = Countdown(timeElapsed: 300.0, timeUpdated: { [weak self] timeInterval in
+        guard let strongSelf = self else { return }
+        if timeInterval == 0 {
+            
+        }
+        strongSelf.updateTimer.value = strongSelf.timeString(from: timeInterval)
+    })
+    
+    private func timeString(from timeInterval: TimeInterval) -> String {
+        let seconds = Int(timeInterval.truncatingRemainder(dividingBy: 60))
+        let minutes = Int(timeInterval.truncatingRemainder(dividingBy: 60 * 60) / 60)
+        return String(format: "%.2d:%.2d", minutes, seconds)
+    }
+    
+    func getChallenge(){
+        showLoadingHud.value = true
+        API.get(Challenge.self, endpoint: .challenge(1), success: { [weak self] (result) in
+            if !result.answer.isEmpty{
+                self!.answer = result.answer
+                self!.onChallengeLoad.value = result.question
+            }
+        }) { [weak self]  (error) in
+            let okAlert = SingleButtonAlert(
+                title: error.localizedDescription,
+                message: "Could not simulate.",
+                action: AlertAction(buttonTitle: "OK", handler: { print("Ok pressed!") })
+            )
+            self!.onShowAlert.value = okAlert.createAlert()
+        }
+    }
+    
+    func startChallenge(){
+        countdown.toggle()
+    }
+    
+    func verifyAnswer(_ answer: String){
+        if self.answer.contains(answer){
+            playerAnswer.append(answer)
+        }
+    }
+    
+    private func gameOver(){
+        
+    }
 }
